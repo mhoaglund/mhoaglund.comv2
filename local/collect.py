0311aeba5b4d9fb6aade59c1e70d4231ffe02734
mhoaglund <maxwell.hoaglund@gmail.com>
2018-11-17 18:50:42 -0600

    more half measures

--- local/collect.py
+++ local/collect.py
@@ -1,1 +1,8 @@
 #TODO: iterate over a folder structure with project stuff in it, and output json.
+
+#Folders: Project -> Components -> Details -> *
+#                 -> Artifacts -> *
+
+#Project folder contains meta.json, which has thesis, materials, themes
+
+#TODO switch for file types or something
